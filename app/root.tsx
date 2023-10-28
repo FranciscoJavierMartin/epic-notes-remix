1ebb1ffcef11a64678ad7b97dd45efcbcfdba47c
Francisco <javiermartincuenca@gmail.com>
2023-10-29 00:09:43 +0200

    Links on root

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
 import os from 'node:os';
 import { json, type LinksFunction } from '@remix-run/node';
 import {
+	Link,
 	Links,
 	LiveReload,
 	Meta,
@@ -40,20 +41,20 @@ export default function App() {
 			<body className='flex h-full flex-col justify-between bg-background text-foreground'>
 				<header className='container mx-auto py-6'>
 					<nav className='flex justify-between'>
-						<div>
+						<Link to='/'>
 							<div className='font-light'>epic</div>
 							<div className='font-bold'>notes</div>
-						</div>
+						</Link>
 					</nav>
 				</header>
 				<div className='flex-1'>
 					<Outlet />
 				</div>
 				<div className='container mx-auto flex justify-between'>
-					<div>
+					<Link to='/'>
 						<div className='font-light'>epic</div>
 						<div className='font-bold'>notes</div>
-					</div>
+					</Link>
 					<p>Built with ♥️ by {data.username}</p>
 				</div>
 				<div className='h-5' />
