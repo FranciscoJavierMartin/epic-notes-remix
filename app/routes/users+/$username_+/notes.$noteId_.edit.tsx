11909bf613cb445b7bc7554a777dbe18fc5e0a25
Francisco <javiermartincuenca@gmail.com>
2023-10-29 21:00:58 +0100

    Edit form

--- app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -1,7 +1,11 @@
+import { json, type DataFunctionArgs } from '@remix-run/node';
+import { Form, useLoaderData } from '@remix-run/react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Textarea } from '@/components/ui/textarea';
 import { db } from '@/utils/db.server';
 import { invariantResponse } from '@/utils/misc';
-import { json, type DataFunctionArgs } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
 
 export async function loader({ params }: DataFunctionArgs) {
 	const note = db.note.findFirst({
@@ -20,5 +24,27 @@ export async function loader({ params }: DataFunctionArgs) {
 export default function NoteEdit() {
 	const data = useLoaderData<typeof loader>();
 
-	return <pre>{JSON.stringify(data, null, 2)}</pre>;
+	return (
+		<Form
+			method='POST'
+			className='flex h-full flex-col gap-y-4 overflow-x-hidden px-10 pb-28 pt-12'
+		>
+			<div className='flex flex-col gap-1'>
+				<div>
+					<Label>Title</Label>
+					<Input name='title' defaultValue={data.note.title} />
+				</div>
+				<div>
+					<Label>Content</Label>
+					<Textarea name='content' defaultValue={data.note.content} />
+				</div>
+			</div>
+			<div className='floating-toolbar'>
+				<Button variant='destructive' type='reset'>
+					Reset
+				</Button>
+				<Button type='submit'>Submit</Button>
+			</div>
+		</Form>
+	);
 }
