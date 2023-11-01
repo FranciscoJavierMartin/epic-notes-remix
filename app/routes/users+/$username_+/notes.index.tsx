545c8c330ae7c98c93e3f86604f7258321c2a7a2
Francisco <javiermartincuenca@gmail.com>
2023-11-01 17:00:46 +0100

    Notes meta tag

--- app/routes/users+/$username_+/notes.index.tsx
+++ app/routes/users+/$username_+/notes.index.tsx
@@ -1,3 +1,28 @@
+import { type MetaFunction } from '@remix-run/react';
+import { type loader as notesLoader } from './notes';
+
+export const meta: MetaFunction<
+	null,
+	{
+		'routes/users+/$username_+/notes': typeof notesLoader;
+	}
+> = ({ data, params, matches }) => {
+	const notesMatch = matches.find(
+		(m) => m.id === 'routes/users+/$username_+/notes',
+	);
+	const displayName = notesMatch?.data.owner.name ?? params.username;
+	const noteCount = notesMatch?.data.notes.length ?? 0;
+	const notesText = noteCount === 1 ? 'note' : 'notes';
+
+	return [
+		{ title: `${displayName}'s Notes | Epic Notes` },
+		{
+			name: 'description',
+			content: `Checkout ${displayName}'s ${noteCount} ${notesText} on Epic Notes`,
+		},
+	];
+};
+
 export default function NotesIndexRoute() {
 	return (
 		<div className='container pt-12'>
