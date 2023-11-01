f0f93f4b7abec7e590c31556acb72b76b8a3deeb
Francisco <javiermartincuenca@gmail.com>
2023-11-01 21:20:30 +0100

    Root error handling

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import os from 'node:os';
 import { json, type LinksFunction } from '@remix-run/node';
 import {
@@ -5,15 +6,17 @@ import {
 	Links,
 	LiveReload,
 	Meta,
+	type MetaFunction,
 	Outlet,
 	Scripts,
 	ScrollRestoration,
 	useLoaderData,
 } from '@remix-run/react';
-import favicon from '@/assets/favicon.svg';
+import { cssBundleHref } from '@remix-run/css-bundle';
+import { GeneralErrorBoundary } from '@/components/error-boundary';
 import fontStylesheetUrl from '@/styles/font.css';
 import tailwindStylesheetUrl from '@/styles/tailwind.css';
-import { cssBundleHref } from '@remix-run/css-bundle';
+import favicon from '@/assets/favicon.svg';
 
 export const links: LinksFunction = () =>
 	[
@@ -27,43 +30,69 @@ export async function loader() {
 	return json({ username: os.userInfo().username });
 }
 
-export default function App() {
-	const data = useLoaderData<typeof loader>();
+export const meta: MetaFunction = () => {
+	return [
+		{ title: 'Epic Notes' },
+		{ name: 'description', content: "Your own captain's log" },
+	];
+};
 
+function Document({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en' className='h-full overflow-x-hidden'>
 			<head>
-				<title>Epic Notes</title>
-				<meta name='description' content="Your own captain's log" />
 				<meta charSet='utf-8' />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<Meta />
 				<Links />
 			</head>
 			<body className='flex h-full flex-col justify-between bg-background text-foreground'>
-				<header className='container mx-auto py-6'>
-					<nav className='flex justify-between'>
-						<Link to='/'>
-							<div className='font-light'>epic</div>
-							<div className='font-bold'>notes</div>
-						</Link>
-					</nav>
-				</header>
-				<div className='flex-1'>
-					<Outlet />
-				</div>
-				<div className='container mx-auto flex justify-between'>
-					<Link to='/'>
-						<div className='font-light'>epic</div>
-						<div className='font-bold'>notes</div>
-					</Link>
-					<p>Built with ♥️ by {data.username}</p>
-				</div>
-				<div className='h-5' />
+				{children}
 				<ScrollRestoration />
 				<Scripts />
 				<LiveReload />
 			</body>
 		</html>
+	);
+}
+
+export default function App({ children }: { children: React.ReactNode }) {
+	const data = useLoaderData<typeof loader>();
+
+	return (
+		<Document>
+			<header className='container mx-auto py-6'>
+				<nav className='flex justify-between'>
+					<Link to='/'>
+						<div className='font-light'>epic</div>
+						<div className='font-bold'>notes</div>
+					</Link>
+				</nav>
+			</header>
+			<div className='flex-1'>
+				<Outlet />
+			</div>
+			<div className='container mx-auto flex justify-between'>
+				<Link to='/'>
+					<div className='font-light'>epic</div>
+					<div className='font-bold'>notes</div>
+				</Link>
+				<p>Built with ♥️ by {data.username}</p>
+			</div>
+			<div className='h-5' />
+			<ScrollRestoration />
+			<Scripts />
+			<LiveReload />
+		</Document>
+	);
+}
+
+export function ErrorBoundary() {
+	return (
+		<Document>
+			<div className='flex-1'>
+				<GeneralErrorBoundary />
+			</div>
+		</Document>
 	);
 }
