00f734d2dcb6d1ef939b4a99caa4feae4274fffe
Francisco <javiermartincuenca@gmail.com>
2023-11-05 16:20:03 +0100

    Notes list

--- app/routes/users+/$username_+/notes.tsx
+++ app/routes/users+/$username_+/notes.tsx
@@ -1,7 +1,7 @@
 import { json, type DataFunctionArgs } from '@remix-run/node';
 import { Link, NavLink, Outlet, useLoaderData } from '@remix-run/react';
 import { GeneralErrorBoundary } from '@/components/error-boundary';
-import { cn, invariantResponse } from '@/utils/misc';
+import { cn, getUserImgSrc, invariantResponse } from '@/utils/misc';
 import { db } from '@/utils/db.server';
 
 export async function loader({ params }: DataFunctionArgs) {
@@ -32,21 +32,23 @@ export async function loader({ params }: DataFunctionArgs) {
 
 export default function NotesRoute() {
 	const data = useLoaderData<typeof loader>();
-	const ownerDisplayName = data.owner.name ?? data.owner.username;
-	const navLinkDefaultClassName =
-		'line-clamp-2 block rounded-l-full py-2 pl-8 pr-6 text-base lg:text-xl';
 
 	return (
-		<main className='container flex h-full min-h-[400px] pb-12 px-0 md:px-8'>
+		<main className='container flex h-full min-h-[400px] px-0 pb-12 md:px-8'>
 			<div className='grid w-full grid-cols-4 bg-muted pl-2 md:container md:mx-2 md:rounded-3xl md:pr-0'>
 				<div className='relative col-span-1'>
 					<div className='absolute inset-0 flex flex-col'>
 						<Link
 							to={`/users/${data.owner.username}`}
-							className='pb-4 pl-8 pr-4 pt-12'
+							className='flex flex-col items-center justify-center gap-2 bg-muted pb-4 pl-8 pr-4 pt-12 lg:flex-row lg:justify-start lg:gap-4'
 						>
-							<h1 className='text-base font-bold md:text-lg lg:text-left lg:text-2xl'>
-								{ownerDisplayName}'s Notes
+							<img
+								src={getUserImgSrc(data.owner.image?.id)}
+								alt={data.owner.name ?? data.owner.username}
+								className='h-16 w-16 rounded-full object-cover lg:h-24 lg:w-24'
+							/>
+							<h1 className='text-center text-base font-bold md:text-lg lg:text-left lg:text-2xl'>
+								{data.owner.name ?? data.owner.username}'s Notes
 							</h1>
 						</Link>
 						<ul className='overflow-y-auto overflow-x-hidden pb-12'>
@@ -57,7 +59,10 @@ export default function NotesRoute() {
 										preventScrollReset
 										prefetch='intent'
 										className={({ isActive }) =>
-											cn(navLinkDefaultClassName, isActive && 'bg-accent')
+											cn(
+												'line-clamp-2 block rounded-l-full py-2 pl-8 pr-6 text-base lg:text-xl',
+												isActive && 'bg-accent',
+											)
 										}
 									>
 										{note.title}
