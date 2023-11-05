868c528fd618a96074c1638d9f5f09ba79ad2722
Francisco <javiermartincuenca@gmail.com>
2023-11-05 13:41:09 +0100

    Add tooltip

--- app/components/ui/status-button.tsx
+++ app/components/ui/status-button.tsx
@@ -1,28 +1,67 @@
 import React from 'react';
+import { useSpinDelay } from 'spin-delay';
 import { type ButtonProps, Button } from './button';
 import { cn } from '@/utils/misc';
+import { Icon } from './icon';
+import { Tooltip, TooltipProvider, TooltipTrigger } from './tooltip';
 
 export const StatusButton = React.forwardRef<
 	HTMLButtonElement,
-	ButtonProps & { status: 'pending' | 'success' | 'error' | 'idle' }
->(({ status = 'idle', className, children, ...props }, ref) => {
-	const companion = {
-		pending: <span className='inline-block animate-spin'>🌀</span>,
-		success: <span>✅</span>,
-		error: <span>❌</span>,
-		idle: null,
-	}[status];
+	ButtonProps & {
+		status: 'pending' | 'success' | 'error' | 'idle';
+		message?: string | null;
+		spinDelay?: Parameters<typeof useSpinDelay>[1];
+	}
+>(
+	(
+		{ message, status = 'idle', className, children, spinDelay, ...props },
+		ref,
+	) => {
+		const delayedPending = useSpinDelay(status === 'pending', {
+			delay: 400,
+			minDuration: 300,
+			...spinDelay,
+		});
 
-	return (
-		<Button
-			ref={ref}
-			className={cn('flex justify-center gap-4', className)}
-			{...props}
-		>
-			<div>{children}</div>
-			{companion}
-		</Button>
-	);
-});
+		const companion = {
+			pending: delayedPending ? (
+				<div className='inline-flex h-6 w-6 items-center justify-center'>
+					<Icon name='update' className='animate-spin' />
+				</div>
+			) : null,
+			success: (
+				<div className='inline-flex h-6 w-6 items-center justify-center'>
+					<Icon name='check' />
+				</div>
+			),
+			error: (
+				<div className='inline-flex h-6 w-6 items-center justify-center rounded-full bg-destructive'>
+					<Icon name='cross-1' className='text-destructive-foreground' />
+				</div>
+			),
+			idle: null,
+		}[status];
+
+		return (
+			<Button
+				ref={ref}
+				className={cn('flex justify-center gap-4', className)}
+				{...props}
+			>
+				<div>{children}</div>
+				{message ? (
+					<TooltipProvider>
+						<Tooltip>
+							<TooltipTrigger>{companion}</TooltipTrigger>
+							<TooltipTrigger>{message}</TooltipTrigger>
+						</Tooltip>
+					</TooltipProvider>
+				) : (
+					companion
+				)}
+			</Button>
+		);
+	},
+);
 
 StatusButton.displayName = 'Button';
