fa42041b0732aa35813928aba127b23a0ca54961
Francisco <javiermartincuenca@gmail.com>
2023-11-05 19:07:38 +0100

    Add errors

--- app/components/ui/image-chooser.tsx
+++ app/components/ui/image-chooser.tsx
@@ -4,7 +4,9 @@ import { type FieldConfig, conform, useFieldset } from '@conform-to/react';
 import { type ImageFieldsetSchema } from '@/routes/users+/$username_+/notes.$noteId_.edit';
 import { Label } from './label';
 import { Textarea } from './textarea';
-import { cn } from '@/utils/misc';
+import { cn, getNoteImgSrc } from '@/utils/misc';
+import { Icon } from './icon';
+import { ErrorList } from '../forms';
 
 export const ImageChooser = ({
 	config,
@@ -15,7 +17,7 @@ export const ImageChooser = ({
 	const fields = useFieldset(ref, config);
 	const existingImage = Boolean(fields.id.defaultValue);
 	const [previewImage, setPreviewImage] = useState<string | null>(
-		existingImage ? `/resources/images/${fields.id.defaultValue}` : null,
+		fields.id.defaultValue ? getNoteImgSrc(fields.id.defaultValue) : null,
 	);
 	const [altText, setAltText] = useState<string>(
 		fields.altText.defaultValue ?? '',
@@ -49,7 +51,7 @@ export const ImageChooser = ({
 								</div>
 							) : (
 								<div className='flex h-32 w-32 items-center justify-center rounded-lg border border-muted-foreground text-4xl text-muted-foreground'>
-									➕
+									<Icon name='plus' />
 								</div>
 							)}
 							{existingImage ? (
@@ -80,6 +82,9 @@ export const ImageChooser = ({
 							/>
 						</label>
 					</div>
+					<div className='min-h-[32px] px-4 pb-3 pt-1'>
+						<ErrorList id={fields.file.errorId} errors={fields.file.errors} />
+					</div>
 				</div>
 				<div className='flex-1'>
 					<Label htmlFor={fields.altText.id}>Alt text</Label>
@@ -87,7 +92,16 @@ export const ImageChooser = ({
 						onChange={(e) => setAltText(e.currentTarget.value)}
 						{...conform.textarea(fields.altText)}
 					/>
+					<div className='min-h-[32px] px-4 pb-3 pt-1'>
+						<ErrorList
+							id={fields.altText.errorId}
+							errors={fields.altText.errors}
+						/>
+					</div>
 				</div>
+			</div>
+			<div className='min-h-[32px] px-4 pb-3 pt-1'>
+				<ErrorList id={config.errorId} errors={config.errors} />
 			</div>
 		</fieldset>
 	);
