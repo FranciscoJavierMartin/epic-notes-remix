76bfbaf4a4f34b6068184d67206c2c7382c57eb0
Francisco <javiermartincuenca@gmail.com>
2023-11-07 22:47:30 +0100

    Prisma client

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -8,7 +8,43 @@ import fs from 'node:fs/promises';
 import os from 'node:os';
 import path from 'node:path';
 import { factory, manyOf, nullable, oneOf, primaryKey } from '@mswjs/data';
+import { PrismaClient } from '@prisma/client';
+import chalk from 'chalk';
 import { singleton } from './singleton.server';
+
+export const prisma = singleton('prisma', () => {
+	const logThreshold = 0;
+
+	const client = new PrismaClient({
+		log: [
+			{ level: 'query', emit: 'event' },
+			{ level: 'error', emit: 'stdout' },
+			{ level: 'info', emit: 'stdout' },
+			{ level: 'warn', emit: 'stdout' },
+		],
+	});
+
+	client.$on('query', async (e) => {
+		if (e.duration >= logThreshold) {
+			const color =
+				e.duration < logThreshold * 1.1
+					? 'green'
+					: e.duration < logThreshold * 1.2
+					? 'blue'
+					: e.duration < logThreshold * 1.3
+					? 'yellow'
+					: e.duration < logThreshold * 1.4
+					? 'redBright'
+					: 'red';
+			const duration = chalk[color](`${e.duration}ms`);
+			console.info(`prisma:query - ${duration} - ${e.query}`);
+		}
+	});
+
+	client.$connect();
+
+	return client;
+});
 
 const getId = () => crypto.randomBytes(16).toString('hex').slice(0, 8);
 
